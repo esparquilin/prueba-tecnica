a0c4a09fd931da576838d1c4840ddfb791c9db03
Angel Chapulin <esparquilin2@gmail.com>
2023-06-01 12:46:53 -0600

    added database challenge

--- Screens/Home.tsx
+++ Screens/Home.tsx
@@ -4,25 +4,76 @@ import { ScrollView, Text, StyleSheet } from "react-native";
 import BankList from "../Components/Banks/BankList";
 
 import LoadingScreen from "../Components/ScreenLoader/LoadingScreen";
+import { BankData } from "../Interfaces/Interfaces";
 
 const Home = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [bankData, setBankData] = useState([]);
 
-  useEffect(() => {
-    setIsLoading(true);
-    const getBankData = async () => {
-      const response = await fetch(
-        "https://dev.obtenmas.com/catom/api/challenge/banks" //This must be an env variable
-      );
-      const responseData = await response.json();
+  const [newDataBase, setNewDataBase] = useState("");
+  const [dataIsFromDB, setDataIsFromDB] = useState(false);
 
-      setBankData(responseData);
-      setIsLoading(false);
+  useEffect(() => {
+    const getBankData = async () => {
+      try {
+        setIsLoading(true);
+
+        const response = await fetch(
+          "https://dev.obtenmas.com/catom/api/challenge/banks" //This must be an env variable
+        );
+        const responseData = await response.json();
+
+        setBankData(responseData);
+        setIsLoading(false);
+
+        const postBankData = async (bankData: any) => {
+          try {
+            const post = await fetch(
+              "https://ejemplo-4103a-default-rtdb.firebaseio.com/.json", //New database
+              {
+                method: "POST",
+                body: JSON.stringify(bankData),
+                headers: {
+                  "Content-Type": "application/json",
+                },
+              }
+            );
+            const responsePost = await post.json();
+            setNewDataBase(responsePost.name);
+          } catch (error) {
+            console.log(error);
+          }
+        };
+
+        postBankData(responseData);
+        setDataIsFromDB(true);
+      } catch (error) {
+        console.log(error);
+      }
     };
 
-    getBankData();
-  }, []);
+    if (!dataIsFromDB) {
+      getBankData();
+    }
+
+    const getBankDataFromNewDB = async () => {
+      try {
+        const response = await fetch(
+          `https://ejemplo-4103a-default-rtdb.firebaseio.com/${newDataBase}/.json`
+        );
+        const responseData = await response.json();
+
+        setBankData(responseData);
+        setIsLoading(false);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    if (newDataBase !== "") {
+      getBankDataFromNewDB();
+    }
+  }, [dataIsFromDB, newDataBase]);
 
   if (isLoading) {
     return <LoadingScreen />;
