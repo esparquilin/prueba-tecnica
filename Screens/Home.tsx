98b2568af469352f7d7f6cf5ac20e3020d5036b1
Angel Chapulin Vargas <118499463+esparquilin@users.noreply.github.com>
2023-06-09 16:43:40 -0600

    modified colors

--- Screens/Home.tsx
+++ Screens/Home.tsx
@@ -1,13 +1,16 @@
 import { useEffect, useState } from "react";
 
-import { ScrollView, Text, StyleSheet } from "react-native";
+import { ScrollView, Text, StyleSheet, useColorScheme } from "react-native";
 import BankList from "../Components/Banks/BankList";
 
 import LoadingScreen from "../Components/ScreenLoader/LoadingScreen";
+import { itemColors } from "../utils/colors";
 
 const Home = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [bankData, setBankData] = useState([]);
+
+  const scheme = useColorScheme();
 
   useEffect(() => {
     setIsLoading(true);
@@ -30,7 +33,16 @@ const Home = () => {
 
   return (
     <ScrollView style={styles.container}>
-      <Text> Escoge el banco de tu preferecia para ver más detalles</Text>
+      <Text
+        style={{
+          color:
+            scheme === "dark"
+              ? itemColors.textDarkTheme
+              : itemColors.textLightTheme,
+        }}
+      >
+        Escoge el banco de tu preferecia para ver más detalles
+      </Text>
       <BankList bankData={bankData} />
     </ScrollView>
   );
